676e22707e6099ad0f2e93a44cbbf00105d2bf92
Doctor Horrible <danieeli@gmail.com>
2013-01-24 18:40:39 +0100

    some modifications in the we project

--- event-sharing-web/src/main/java/com/eventsharing/bean/EventsSearchBean.java
+++ event-sharing-web/src/main/java/com/eventsharing/bean/EventsSearchBean.java
@@ -1,8 +1,26 @@
 package com.eventsharing.bean;
 
+import java.util.List;
+
 import javax.enterprise.inject.Model;
+import javax.xml.stream.EventFilter;
+
+import com.eventsharing.entity.Event;
 
 @Model
 public class EventsSearchBean {
 
+	private EventFilter filter;
+	private List<Event> results;
+
+	public EventFilter getFilter() {
+		return filter;
+	}
+
+	public void setFilter(EventFilter filter) {
+		this.filter = filter;
+	}
+	
+	
+	
 }
