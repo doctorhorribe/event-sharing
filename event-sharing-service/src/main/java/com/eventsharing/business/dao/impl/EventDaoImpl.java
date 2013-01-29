9c7cee26b76e9f0f5b6c4170d75c54a719fd0d76
Doctor Horrible <danieeli@gmail.com>
2013-01-29 08:15:10 +0100

    some small changes

--- event-sharing-service/src/main/java/com/eventsharing/business/dao/impl/EventDaoImpl.java
+++ event-sharing-service/src/main/java/com/eventsharing/business/dao/impl/EventDaoImpl.java
@@ -3,8 +3,6 @@ package com.eventsharing.business.dao.impl;
 import java.util.List;
 
 import javax.ejb.Stateless;
-import javax.ejb.TransactionAttribute;
-import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceContextType;
--- event-sharing-web/src/main/java/com/eventsharing/bean/EventsSearchBean.java
+++ event-sharing-web/src/main/java/com/eventsharing/bean/EventsSearchBean.java
@@ -3,8 +3,11 @@ package com.eventsharing.bean;
 import java.util.List;
 
 import javax.enterprise.inject.Model;
-import javax.xml.stream.EventFilter;
+import javax.inject.Inject;
 
+
+import com.eventsharing.business.dao.EventDao;
+import com.eventsharing.business.filter.EventFilter;
 import com.eventsharing.entity.Event;
 
 @Model
@@ -13,12 +16,31 @@ public class EventsSearchBean {
 	private EventFilter filter;
 	private List<Event> results;
 
+	@Inject EventDao eventDao; 
+	
+	public EventsSearchBean() {
+		this.filter = new EventFilter();
+	}
+	
 	public EventFilter getFilter() {
 		return filter;
 	}
 
 	public void setFilter(EventFilter filter) {
 		this.filter = filter;
+	}
+	
+	public List<Event> search() {
+		this.results = eventDao.findEvents(this.filter);
+		return results;
+	}
+
+	public List<Event> getResults() {
+		return results;
+	}
+
+	public void setResults(List<Event> results) {
+		this.results = results;
 	}
 	
 	
